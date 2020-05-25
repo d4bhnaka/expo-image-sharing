801e9dd01e1d09346344df0da329dc494d4364ee
DESIGN4B <staff@design4b.co.jp>
2020-05-25 18:39:12 +0900

    pick image

--- App.tsx
+++ App.tsx
@@ -1,10 +1,54 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import logo from './assets/logo.png';
+import * as ImagePicker from 'expo-image-picker';
+
+type SelectedImage = {
+  localUri: string
+};
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState<SelectedImage|null>(null);
+
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    console.log(pickerResult);
+
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setSelectedImage({ localUri: pickerResult.uri });
+  };
+
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image source={{ uri: selectedImage.localUri }} style={styles.thumbnail} />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={{ width: 305, height: 159 }} />
+      <Image source={logo} style={styles.logo}/>
+
+      <Text style={styles.instructions}>To share a photo from your phone with a friend, just press the button below!</Text>
+
+      <TouchableOpacity
+        onPress={openImagePickerAsync}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -16,4 +60,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width: 100,
+    height: 100,
+    margin: 20
+  },
+  button: {
+    backgroundColor: '#0d6ec5',
+    paddingVertical: 10,
+    paddingHorizontal: 30
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff'
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+    marginBottom: 20
+  },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain"
+  }
 });
---
+++ type/index.d.ts
@@ -0,0 +1,5 @@
+import "./jpg";
+import "./png";
+import "./md";
+import "./mp4";
+import "./svg";
